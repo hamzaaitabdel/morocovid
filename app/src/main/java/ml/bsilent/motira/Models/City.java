49119853c9afb54a7a489b90664f0b76724b76e6
abdellah711 <alaouiabdellah711@gmail.com>
2020-04-08 16:35:27 +0200

    corona

--- app/src/main/java/ml/bsilent/motira/Models/City.java
+++ app/src/main/java/ml/bsilent/motira/Models/City.java
@@ -3,7 +3,7 @@ package ml.bsilent.motira.Models;
 import com.google.android.gms.maps.model.LatLng;
 
 public class City {
-    private String city;
+    private String city,name;
     private int num;
     private double x,y;
 
@@ -15,7 +15,15 @@ public class City {
         return y;
     }
 
+    public void setNum(int num) {
+        this.num = num;
+    }
+
     public City(){}
+
+    public String getName() {
+        return name;
+    }
 
     public City(String city, int num, LatLng latLng) {
         this.city = city;
@@ -23,7 +31,23 @@ public class City {
         this.x = latLng.latitude;
         this.y=latLng.longitude;
     }
-    public City(String city, int num, float x,float y) {
+
+    public City(String city, String name, int num, double x, double y) {
+        this.city = city;
+        this.name = name;
+        this.num = num;
+        this.x = x;
+        this.y = y;
+    }
+    public City( String name,String city, double x, double y) {
+        this.city = city;
+        this.name = name;
+        this.num = 0;
+        this.x = x;
+        this.y = y;
+    }
+
+    public City(String city, int num, float x, float y) {
         this.city = city;
         this.num = num;
         this.x = x;
--- app/src/main/java/ml/bsilent/motira/UI/MainActivity.java
+++ app/src/main/java/ml/bsilent/motira/UI/MainActivity.java
@@ -1,24 +1,24 @@
 package ml.bsilent.motira.UI;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.LinearSmoothScroller;
-import androidx.recyclerview.widget.LinearSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.PointF;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,34 +35,46 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import ml.bsilent.motira.Adapters.CitiesAdapter;
 import ml.bsilent.motira.Models.City;
 import ml.bsilent.motira.Models.Info;
 import ml.bsilent.motira.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<City>> {
     private SupportMapFragment mapFragment;
     private GoogleMap map;
     private RecyclerView recyclerView;
-    private ArrayList<City> cities = new ArrayList<>();
-    private TextView death,recovered,total,new_case,excluded;
+    private static ArrayList<City> cities = new ArrayList<>();
+    private static TextView death,recovered,total,new_case,excluded;
     private Button akhbar;
-    private long tot=0;
+    private static long tot=0;
+    private String Url="https://moroccostats.herokuapp.com/stats/coronavirus/countries/morocco/regions";
     private SwipeRefreshLayout refreshLayout;
     private CitiesAdapter adapter;
-    DatabaseReference databaseRef;
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FrameLayout progress;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        databaseRef =database.getReference();
+        initCities();
         setContentView(R.layout.activity_main);
         death=findViewById(R.id.death);
+        progress=findViewById(R.id.progress);
         refreshLayout = findViewById(R.id.refresh);
         excluded=findViewById(R.id.new_death);
         total=findViewById(R.id.total);
@@ -78,20 +90,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         refreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorPrimaryDark));
-        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                getDataFromDB();
-            }
-        });
+
         adapter =new CitiesAdapter(this,cities);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        getDataFromDB();
         recyclerView=findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
-        OnMapReadyCallback callback=new OnMapReadyCallback() {
+        final Bundle bundle= new Bundle();
+        bundle.putString("aa", Url);
+        final LoaderManager loaderManager = getSupportLoaderManager();
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loaderManager.restartLoader(21,bundle,MainActivity.this);
+            }
+        });
+        if(loaderManager==null) {
+            loaderManager.initLoader(21, bundle, this);
+        }else {
+            loaderManager.restartLoader(21,bundle,this);
+        }
+                OnMapReadyCallback callback=new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 map= googleMap;
@@ -99,77 +119,12 @@ public class MainActivity extends AppCompatActivity {
                 map.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this,R.raw.map));
                 map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28, -4),5.3f));
                 map.getUiSettings().setAllGesturesEnabled(false);
-                for(City city : cities){
-                    googleMap.addCircle(new CircleOptions().radius(city.getNum()*600000/tot).center(new LatLng(city.getX(),city.getY())).fillColor(Color.parseColor("#70ff4c4c")).strokeWidth(0));
-                }
             }
         };
         mapFragment.getMapAsync(callback);
         mapFragment.getView().setClickable(false);
     }
 
-    private void getDataFromDB() {
-        cities.clear();
-        tot=0;
-        databaseRef.child("others").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot info1: dataSnapshot.getChildren()){
-                    Info info =info1.getValue(Info.class);
-                    death.setText(info.getDeath()+"");
-                    excluded.setText(info.getExcluded()+"");
-                    new_case.setText(info.getNews()+"");
-                    recovered.setText(info.getRecoverers()+"");
-
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        databaseRef.child("Cities").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot city1: dataSnapshot.getChildren()){
-                    City city =city1.getValue(City.class);
-                    cities.add(city);
-                    tot+=city.getNum();
-                }
-                total.setText(tot+"");
-
-                if(cities.size()==dataSnapshot.getChildrenCount()){
-                    if(refreshLayout.isRefreshing()){
-                        refreshLayout.setRefreshing(false);
-                    }
-                    adapter.updateData(cities);
-                    OnMapReadyCallback callback=new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            map= googleMap;
-                            map.clear();
-                            map.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this,R.raw.map));
-                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28, -4),5.3f));
-                            map.getUiSettings().setAllGesturesEnabled(false);
-                            for(City city : cities){
-                                googleMap.addCircle(new CircleOptions().radius(city.getNum()*500000/tot).center(new LatLng(city.getX(),city.getY())).fillColor(Color.parseColor("#70ff4c4c")).strokeWidth(0));
-                            }
-
-                        }
-                    };
-                    mapFragment.getMapAsync(callback);
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(MainActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -191,5 +146,174 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default: return super.onOptionsItemSelected(item);
         }
+    }
+    private static URL createUrl(String url){
+        URL Url = null;
+        try{
+            Url = new URL(url);
+        }catch (IOException e){
+        }
+        return Url;
+    }
+    private static String makeHttpRequest(URL url) throws IOException {
+        String json="";
+        if(url == null){
+            return json;
+        }
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if(urlConnection.getResponseCode() == 200){
+                inputStream = urlConnection.getInputStream();
+                json = readFromStream(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if(inputStream !=null){
+                inputStream.close();
+            }
+            return json;
+        }
+    }
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        if(inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream , Charset.forName("UTF-8"));
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = bufferedReader.readLine();
+            while (line != null){
+                sb.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        return sb.toString();
+    }
+    private static ArrayList<City> getCitiesInfo(String json) throws JSONException {
+        ArrayList<City> c=cities;
+        tot=0;
+        JSONObject object = new JSONObject(json);
+        Iterator<String> iterator = object.keys();
+        while (iterator.hasNext()){
+            String region = iterator.next();
+            int cases=Integer.parseInt(object.getString(region));
+            for(City city : c){
+                if(city.getName().equals(region)){
+                    city.setNum(cases);
+                    tot+=cases;
+                }
+            }
+        }
+        return c;
+    }
+
+    @NonNull
+    @Override
+    public androidx.loader.content.Loader<List<City>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new Loader(this,args.getString("aa"));
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull androidx.loader.content.Loader<List<City>> loader, List<City> data) {
+        adapter.updateData((ArrayList<City>) data);
+        cities= (ArrayList<City>) data;
+        progress.setVisibility(View.GONE);
+        total.setText(tot+"");
+        if(refreshLayout.isRefreshing()){
+        refreshLayout.setRefreshing(false);}
+        OnMapReadyCallback callback=new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map= googleMap;
+                map.clear();
+                map.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this,R.raw.map));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(28, -4),5.3f));
+                map.getUiSettings().setAllGesturesEnabled(false);
+                for(City city : cities){
+                    googleMap.addCircle(new CircleOptions().radius(city.getNum()*500000/tot).center(new LatLng(city.getX(),city.getY())).fillColor(Color.parseColor("#70ff4c4c")).strokeWidth(0));
+                }
+
+            }
+        };
+        mapFragment.getMapAsync(callback);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull androidx.loader.content.Loader<List<City>> loader) {
+    adapter.updateData(null);
+    }
+
+    static class Loader extends androidx.loader.content.AsyncTaskLoader<List<City>>{
+        String url;
+        public Loader(Context context,String url) {
+            super(context);
+            this.url=url;
+        }
+        @Override
+        protected void onStartLoading() {
+            super.onStartLoading();
+            forceLoad();
+        }
+        @Override
+        public List<City> loadInBackground() {
+            if(url==null){
+                return null;
+            }else {
+                try {
+                    ArrayList<City> c = getRegionsDetails(url);
+                    URL url = createUrl("https://moroccostats.herokuapp.com/stats/coronavirus/countries/morocco/");
+                    String json = makeHttpRequest(url);
+                    updateInfo(json);
+                    return c;
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+    }
+    private static void updateInfo(String json) throws JSONException {
+        JSONObject object=new JSONObject(json);
+        new_case.setText(object.getString("newcases"));
+        excluded.setText(object.getString("TotalTests"));
+        death.setText(object.getString("totaldeaths"));
+        recovered.setText(object.getString("recovered"));
+    }
+    public void initCities(){
+        cities.add(new City("BeniMellalKhnifra","BeniMellal-Khénifra", 32.33725, -6.34983));
+        cities.add(new City("Daraatafilalet","Drâa-Tafilalet", 31.75,4.5 ));
+        cities.add(new City("Fsmeknes","Fes-Meknes",34.03715 ,-4.9998 ));
+        cities.add(new City("LayouneSakiaElHamra","Layoune-SakiaElHamra",27.1418000 ,-13.1879700 ));
+        cities.add(new City("GuelmimOuedNoun","Guelmim-Oued Noun",28.98696 ,-10.05738 ));
+        cities.add(new City("MarrakechSafi","Marrakech-safi",31.63 , -8.008889));
+        cities.add(new City("Oriental","Oriental",34.686667 ,-1.911389 ));
+        cities.add(new City("SoussMassa","Souss-Massa",31.120018,-8.067919 ));
+        cities.add(new City("RabatSalKenitra","Rabat-Sale-Kenitra",34.020882 , -6.84165));
+        cities.add(new City("TangerTetouanAlHoceima","Tanger-Tetouan",35.5889 ,-5.3626 ));
+        cities.add(new City("CasaSettat","Casa-settat",33.5883100,-7.6113800));
+        cities.add(new City("DakhlaOuedEdDahab","Dakhla-Oued-EdDahab",23.6847700, -15.9579800));
+    }
+    private static ArrayList<City> getRegionsDetails(String link) throws IOException, JSONException {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        URL url = createUrl(link);
+        String json = makeHttpRequest(url);
+        ArrayList<City> arrayList = getCitiesInfo(json);
+        return arrayList;
     }
 }
